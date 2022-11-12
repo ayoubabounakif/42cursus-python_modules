a48ba5d5405fb2921f3067fc12a86376f4780d60
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2022-11-12 17:16:59 +0100

    [MODULE_00][EX02] Implement whoispy program

---
+++ module_00/ex02/whoispy.py
@@ -0,0 +1,11 @@
+import sys
+
+try:
+    if (len(sys.argv) - 1 == 1):
+        num = int(sys.argv[1])
+        outputs = ['Even', 'Odd', 'Zero']
+        print("I'm " + outputs[2] + '.' if int(num) == 0 else "I'm " + outputs[0] + '.' if int(num) % 2 == 0 else ("I'm " + outputs[1] + '.'))
+    else:
+        print("AssertionError: more than one argument are provided")
+except:
+    print("AssertionError: argument is not an integer")
