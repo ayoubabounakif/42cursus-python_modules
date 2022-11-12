c2dda51a9c9e4d3b36e4f475c33df28501db5c93
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2022-11-12 17:21:24 +0100

    [MODULE_00][EX02] Improve error message

--- module_00/ex02/whois.py
+++ module_00/ex02/whois.py
@@ -6,6 +6,6 @@ try:
         outputs = ['Even', 'Odd', 'Zero']
         print("I'm " + outputs[2] + '.' if int(num) == 0 else "I'm " + outputs[0] + '.' if int(num) % 2 == 0 else ("I'm " + outputs[1] + '.'))
     else:
-        print("AssertionError: more than one argument are provided")
+        print("AssertionError: more than one argument are provided" if len(sys.argv) - 1 > 1 else "Arguments expected\nusage: python3 whois.py [numeric_arg]")
 except:
     print("AssertionError: argument is not an integer")
