5b83750095f4f16b1237d1718ecdc737df781ae8
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2022-11-19 16:43:14 +0100

    [PYTHON][M_00] Implement recipe.py

--- module_00/ex06/recipe.py
+++ module_00/ex06/recipe.py
@@ -1,35 +1,107 @@
-meals = ['lunch', 'dessert']
-
-dish = {
-    'eggs': 2, 'sausage': 1, 'bacon': 1, 'spam': 500
-}
-
-for tup in zip(dish.keys(), dish.values()):
-    print(tup[0])
-
 cookbook = {
     'sandwich' : {
         'ingredients' : ["ham", "bread", "cheese", "tomatoes"],
-        'meal' : meals[0],
+        'meal' : 'lunch',
         'prep_time': 10,
     },
     'cake': {
-        'ingredients' : ["flour", "sugar", "eggs", "tomatoes"],
-        'meal': meals[1],
+        'ingredients' : ["flour", "sugar", "eggs"],
+        'meal': 'dessert',
         'prep_time': 60,
     },
     'salad': {
         'ingredients' : ['avocado', 'arugula', 'tomatoes', 'spinach'],
-        'meal': meals[0],
+        'meal': 'lunch',
         'prep_time': 15,
     }
 }
 
-# print(cookbook.items())
-# pairs = zip(cookbook.keys(), cookbook.values())
-# print(list(pairs)[2][1])
+# A series of Helpful Function
 
-# def print_all_recipe_names(cookbook):
-#     print(cookbook[0]['ingredients'])
+def print_cookbook():
+    print(list(cookbook))
 
-# print_all_recipe_names(cookbook)
+def print_recipe_details(recipe_name = None):
+    if (recipe_name == None):
+        recipe_name = input("Please enter a recipe name to get its details:\n>> ")
+    print('\n', end="")
+    try:
+        print('Recipe for {}:'.format(recipe_name))
+        print('     Ingredients list: {}'.format(list(cookbook[recipe_name]['ingredients'])))
+        print('     To be eaten for {}.'.format(cookbook[recipe_name]['meal']))
+        print('     Takes {} minutes of cooking.'.format(cookbook[recipe_name]['prep_time']))
+    except KeyError:
+        base_error('This recipe is not available on the cookbook')
+
+def delete_recipe(recipe_name = None):
+    if (recipe_name == None):
+        recipe_name = input("Please enter a recipe name to delete:\n>> ")
+    print('\n', end="")
+    try:
+        del cookbook[recipe_name] 
+        print(recipe_name, 'deleted successfuly!')
+    except KeyError:
+        base_error('This recipe is not available on the cookbook')
+
+def add_recipe():
+    try:
+        recipe_name = input('Enter a name:\n>> ')
+        ingredients = []
+        print("\nEnter ingredients:")
+        while True:
+            inp = input()
+            if not inp:
+                break
+            ingredients.append(inp)
+        meal_type = input('\nEnter a meal type:\n')
+        prep_time = int(input('\nEnter a preparation time:\n'))
+        while prep_time < 0:
+            prep_time = int(input('\nPlease enter a non-negative integer:\n'))
+        cookbook.update({
+        recipe_name: {
+            'ingredients': list(ingredients),
+            'meal': meal_type,
+            'prep_time': prep_time,
+        }})
+    except Exception as error:
+        base_error(error)
+
+def quit_program():
+    print('Cookbook closed. Goodbye !')
+    exit()
+
+def base_error(error_message = 'Sorry, this option does not exist.'):
+    print(error_message)
+    options_list()
+
+def options_list():
+    print("List of available option:\n \
+    1: Add a recipe\n \
+    2: Delete a recipe\n \
+    3: Print a recipe\n \
+    4: Print the cookbook\n \
+    5: Quit \
+")
+
+def option_chosen(arg):
+    switcher = {
+        '1': add_recipe,
+        '2': delete_recipe,
+        '3': print_recipe_details,
+        '4': print_cookbook,
+        '5': quit_program,
+    }
+    return switcher.get(arg, base_error)
+
+def main():
+    print("Welcome to the Python cookbook !")
+    options_list()
+    while True:
+        option = input('\nPlease select an option:\n>> ')
+        func_to_execute = option_chosen(option)
+        print('\n', end="")
+        func_to_execute()
+
+if __name__ == '__main__':
+    main()
+    
