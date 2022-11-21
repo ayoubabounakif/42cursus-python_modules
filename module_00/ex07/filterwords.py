2e697075946b49119a693541440a7a08259e25d5
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2022-11-21 10:42:17 +0100

    [PYTHON][M_00] Implement filterwords.py & sos.py

--- module_00/ex07/filterwords.py
+++ module_00/ex07/filterwords.py
@@ -2,20 +2,18 @@
 # of words in S that contains more than N non-punctuation characters.
 
 from sys import argv
+import string
 
-def check_argv_specs(args):
-    if len(args) == 2:
-        return
-    try:
-        # s
-    except:
-        raise 'ERROR'
-    
+def check_argv_specs(argv):
+    if (len(argv) != 2) or (argv[0].isdigit() == True or argv[1].isdigit() == False):
+        raise Exception()
 
 if __name__ == '__main__':
     try:
-        check_argv_specs(argv[1:])
+        argv = argv[1:]
+        check_argv_specs(argv)
+        argv[0] = argv[0].translate(str.maketrans('', '', string.punctuation))
+        new_list = [x for x in argv[0].split() if len(x) > int(argv[1])]
+        print(new_list)
     except Exception as error:
-        print(error)
-    arg_check = True if len(argv) != 2 else True if argv[0].isdigit() == False and argv[1].isdigit() else False
-    print('ERROR' if arg_check else '')
+        print('ERROR')
