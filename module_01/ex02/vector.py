e9e620a1483713a6873e250d1db87d3f09b6db48
ABOUNAKIF Ayoub <aabounakif@leyton.com>
2022-11-30 15:46:17 +0100

    [PTYHON][M_01] Improve on the logic behind my constructor and should fix magic methods

--- module_01/ex02/vector.py
+++ module_01/ex02/vector.py
@@ -16,36 +16,25 @@ class Vector:
     def __init__(self, values):
         self.__checkvalidity(values)
         if isinstance(values, int):
-            # assert values >= 0, f"Value should be positive"
-            self.values = []
-            for x in range(values):
-                self.values.append([float(x)])
-            self.shape = (len(self.values), 1)
+            self.values = [[float(x)] for x in range(values)]
+            self.shape = (values, 1)
         elif isinstance(values, tuple):
-            self.values = []
-            for x in range(values[0], values[1]):
-                self.values.append([float(x)])
-            self.shape = (len(self.values), 1)
+            self.values = [[float(x)] for x in range(values[0], values[1])]
+            self.shape = (values[1] - values[0], 1)
         else:
-            self.values = []
-            inc_flag = 0
-            if isinstance(values, list):
-                for x in range(len(values)):
-                    if isinstance(values[x], list):
-                        for i in range(len(values[x])):
-                            assert isinstance(values[x][i], (int, float)), f"should only construct a list with numbers"
-                            values[x][i] = float(values[x][i])
-                            inc_flag += 1
-                        self.shape = (len(values), 1) if len(values) != 1 else (1, inc_flag)
-                    elif isinstance(values[x], (int, float)):
-                        self.shape = (1, len(values))
-                        assert isinstance(values[x], (int, float)), f"should only construct a list with numbers" 
-                        values[x] = float(values[x])
-            self.values = values
+            self.values = [list(map(float, sublist)) for sublist in values]
+            self.shape = (len(self.values), len(self.values[0]))
+
+    def __error(self, other, type, op):
+        if (type == "Vector"):
+            if not isinstance(other, Vector): raise NotImplementedError(f"should only {op} a Vector to a Vector")
+            if not self.shape == other.shape: raise ValueError(f"should only {op} a Vector of same shape")
+        elif (type == "scalar"):
+            if not isinstance(other, (int, float)): raise NotImplementedError(f"should only {op} a Vector to a scalar")
+            if (op == "divide" and other == 0): raise ValueError(f"should only {op} a Vector to a non-zero scalar")
 
     def __add__(self, other):
-        assert isinstance(other, Vector), f"should only add a Vector to a Vector"
-        assert self.shape == other.shape, f"should only add a Vector of same shape"
+        self.__error(other, "Vector", "add")
         result = []
         for x in range(len(self.values)):
             result.append([self.values[x][0] + other.values[x][0]])
@@ -55,8 +44,7 @@ class Vector:
         return self.__add__(other)
 
     def __sub__(self, other):
-        assert isinstance(other, Vector), f"should only subtract a Vector from a Vector"
-        assert self.shape == other.shape, f"should only subtract a Vector of same shape"
+        self.__error(other, "Vector", "subtract")
         result = []
         for x in range(len(self.values)):
             result.append([self.values[x][0] - other.values[x][0]])
@@ -66,7 +54,7 @@ class Vector:
         return self.__sub__(other)
 
     def __truediv__(self, other):
-        assert isinstance(other, (int, float)) and other != 0, NotImplementedError("should only divide a Vector by a scalar")
+        self.__error(other, "scalar", "divide")
         result = []
         for x in range(len(self.values)):
             result.append([self.values[x][0] / other])
@@ -76,7 +64,7 @@ class Vector:
         return self.__truediv__(other)
     
     def __mul__(self, other):
-        assert isinstance(other, (int, float)), NotImplementedError("should only divide a Vector by a scalar")
+        self.__error(other, "scalar", "multiply")
         result = []
         for x in range(len(self.values)):
             result.append([self.values[x][0] * other])
@@ -93,53 +81,41 @@ class Vector:
 
 
 if __name__ == '__main__':
-    # # ------------------------------------
-    # print(Vector([1. , 2e-3, 3.14, 5]).shape) # [1.0, 2e-3, 3.14, 5.0]
 
-    # print(Vector(4).values) # [[0.0], [1.0], [2.0], [3.0]]
-    # print(Vector(4).shape)
-    # # print(Vector(-1).values) # This should be an error
-    # print(Vector([[0.0, 1.0, 2.0, 5]]).values) # [[0.0, 1.0, 2.0, 5.0]]
-    # print(Vector([[0.0], [1.0], [2.0], [3.0]]).values) # [[0.0], [1.0], [2.0], [3.0]]
-    # print(Vector((10, 12)).values) # [[10.0], [11.0]]
-    # print(Vector((1, 1)).values) # []
-    # print(Vector((4, 7.1)).values) # Cannot interpret floats as int
+    print(Vector(4).values) # [[0.0], [1.0], [2.0], [3.0]]
+    print(Vector(4).shape) # (4, 1)
 
-    # # ------------------------------------
-    # Column vector of shape (n, 1)
-    # print(Vector([[0.0], [1.0], [2.0], [3.0]]).shape)
-    # Expected output
-    # (4,1)
+    print(Vector((2, 4)).values) # [[2.0], [3.0]]
+    print(Vector((2, 4)).shape) # (2, 1)
 
-    # print(Vector([[0.0], [1.0], [2.0], [3.0]]).values)
-    # Expected output
-    # [[0.0], [1.0], [2.0], [3.0]]
+    print(Vector([[1, 2, 3], [4, 5, 6]]).values) # [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]
+    print(Vector([[1, 2, 3], [4, 5, 6]]).shape) # (2, 3)
 
-    # Row vector of shape (1, n)
-    # print(Vector([[0.0, 1.0, 2.0, 3.0]]).shape)
-    # Expected output
-    # (1,4)
+    print(Vector([[1, 2, 3], [4, 5, 6]]).values[0]) # [1.0, 2.0, 3.0]
+    print(Vector([[1, 2, 3], [4, 5, 6]]).values[0][0]) # 1.0
 
-    # v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
-    # print(v1.shape)
-    # # Expected output
-    # # [[0.0, 1.0, 2.0, 3.0]]
+    # v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+    # v2 = Vector([[0.0], [1.0], [2.0], [0.0]])
+
+    # v3 = Vector([[0.0, 1.0, 2.0, 3.0]])
+    # v4 = Vector([[0.0, 1.0, 1.0, 0.0]])
+
+    # try:
+    #     print(v1.__add__(v2).values)
+    #     print(v1.__sub__(v2).values)
+    #     print(v1.__mul__(0).values)
+    #     print(v1.__truediv__(5).values)
+    # except Exception as error:
+    #     print(error)
+        
+    # try:
+    #     print(v3.__add__(v4).values)
+    #     print(v3.__sub__(v4).values)
+    #     print(v3.__mul__(0).values)
+    #     print(v3.__truediv__(4).values)
+    # except Exception as error:
+    #     print(error)
 
 
-    v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
-    print(v1.values)
-
-    # # Example 1:
-    v2 = Vector([[0.0], [1.0], [2.0], [3.0]])
-    print(v2.values)
-    # Expected output:
-    # (4,1)
-
-    # print(v1.__add__(v2).values)
-    # print(v1.__sub__(v2).values)
-    try:
-        print(v1.__mul__(v2).values)
-    except Exception as error:
-        print(type(error), error)
-    # print(v1.__truediv__(0).values)
+    
     
