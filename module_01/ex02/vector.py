c967970f2d6bb59abbb70e9fa8ba91aadc9b0d01
ABOUNAKIF Ayoub <aabounakif@leyton.com>
2022-11-30 15:09:27 +0100

    [PYTHON][M_01] Vector implemented

--- module_01/ex02/vector.py
+++ module_01/ex02/vector.py
@@ -13,8 +13,6 @@ class Vector:
         else:
             pass
 
-
-
     def __init__(self, values):
         self.__checkvalidity(values)
         if isinstance(values, int):
@@ -45,18 +43,61 @@ class Vector:
                         values[x] = float(values[x])
             self.values = values
 
-    def dot(self):
-        print('Dot product')
+    def __add__(self, other):
+        assert isinstance(other, Vector), f"should only add a Vector to a Vector"
+        assert self.shape == other.shape, f"should only add a Vector of same shape"
+        result = []
+        for x in range(len(self.values)):
+            result.append([self.values[x][0] + other.values[x][0]])
+        return Vector(result)
 
-    def T(self):
-        print('Vector Transpose')
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        assert isinstance(other, Vector), f"should only subtract a Vector from a Vector"
+        assert self.shape == other.shape, f"should only subtract a Vector of same shape"
+        result = []
+        for x in range(len(self.values)):
+            result.append([self.values[x][0] - other.values[x][0]])
+        return Vector(result)
+    
+    def __rsub__(self, other):
+        return self.__sub__(other)
+
+    def __truediv__(self, other):
+        assert isinstance(other, (int, float)) and other != 0, NotImplementedError("should only divide a Vector by a scalar")
+        result = []
+        for x in range(len(self.values)):
+            result.append([self.values[x][0] / other])
+        return Vector(result)
+    
+    def __rtruediv__(self, other):
+        return self.__truediv__(other)
+    
+    def __mul__(self, other):
+        assert isinstance(other, (int, float)), NotImplementedError("should only divide a Vector by a scalar")
+        result = []
+        for x in range(len(self.values)):
+            result.append([self.values[x][0] * other])
+        return Vector(result)
+    
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __str__(self):
+        return f"Vector({self.values})"
+    
+    def __repr__(self):
+        return f"Vector({self.values})"
+
 
 if __name__ == '__main__':
     # # ------------------------------------
     # print(Vector([1. , 2e-3, 3.14, 5]).shape) # [1.0, 2e-3, 3.14, 5.0]
 
-    print(Vector(4).values) # [[0.0], [1.0], [2.0], [3.0]]
-    print(Vector(4).shape)
+    # print(Vector(4).values) # [[0.0], [1.0], [2.0], [3.0]]
+    # print(Vector(4).shape)
     # # print(Vector(-1).values) # This should be an error
     # print(Vector([[0.0, 1.0, 2.0, 5]]).values) # [[0.0, 1.0, 2.0, 5.0]]
     # print(Vector([[0.0], [1.0], [2.0], [3.0]]).values) # [[0.0], [1.0], [2.0], [3.0]]
@@ -79,14 +120,26 @@ if __name__ == '__main__':
     # Expected output
     # (1,4)
 
-    v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
-    print(v1.shape)
+    # v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
+    # print(v1.shape)
     # # Expected output
     # # [[0.0, 1.0, 2.0, 3.0]]
 
-    # # Example 1:
+
     v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
-    print(v1.shape)
+    print(v1.values)
+
+    # # Example 1:
+    v2 = Vector([[0.0], [1.0], [2.0], [3.0]])
+    print(v2.values)
     # Expected output:
     # (4,1)
+
+    # print(v1.__add__(v2).values)
+    # print(v1.__sub__(v2).values)
+    try:
+        print(v1.__mul__(v2).values)
+    except Exception as error:
+        print(type(error), error)
+    # print(v1.__truediv__(0).values)
     
