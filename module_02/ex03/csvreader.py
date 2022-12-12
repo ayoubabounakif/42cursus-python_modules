f56633f217ac91a67076ef54da96318358a58264
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2022-12-12 11:33:14 +0100

    [PYTHON][M_02] Improvement on CsvReader + Tests

--- module_02/ex03/csvreader.py
+++ module_02/ex03/csvreader.py
@@ -16,37 +16,32 @@ class CsvReader:
         self.header_row = None
         self.num_fields = None
 
-        # Skip the top rows, if necessary
+        if self.header:
+            self.header_row = self.file.readline().strip().split(self.sep)
+        # else:
+        #     self.file.readline().strip().split(self.sep)
+
         for _ in range(self.skip_top):
             self.file.readline()
 
-        # Read the header row, if necessary
-        if self.header:
-            self.header_row = self.file.readline().strip().split(self.sep)
-
-        # Parse the remaining rows
         for line in self.file:
             fields = line.strip().split(self.sep)
             if self.num_fields is None:
                 self.num_fields = len(fields)
             elif len(fields) != self.num_fields:
-                # Inconsistent number of fields; file is corrupted
                 return None
             self.data.append(fields)
 
-        # Skip the bottom rows, if necessary
         for _ in range(self.skip_bottom):
             self.data.pop()
 
         return self
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, exception_type, exception_value, exception_traceback):
         try:
             self.file.close()
         except AttributeError:
             return None
-        
-
 
     def getdata(self):
         return self.data
--- module_02/ex03/main.py
+++ module_02/ex03/main.py
@@ -8,20 +8,23 @@ if __name__ == "__main__":
     #         print("File is corrupted or missing")
     #     else:
     #         print(reader.getheader(), end = "\n")
-    #         print(reader.getdata(), end = "\n\n")
+    #         for i in range(len(reader.getdata())):
+    #             print(reader.getdata()[i], end = "\n")
 
-    with CsvReader(filename, header = True, skip_top=0, skip_bottom=0) as reader:
+    with CsvReader(filename, header=False, skip_top=0, skip_bottom=0) as reader:
         if reader == None:
             print("File is corrupted or missing")
         else:
-            # print(reader.getheader(), end = "\n")
-            print(reader.getdata(), end = "\n")
+            print(reader.getheader(), end = "\n")
+            for i in range(len(reader.getdata())):
+                print(reader.getdata()[i], end = "\n")
 
-# if __name__ == "__main__":
-#     with CsvReader('good.csv') as file:
-#         data = file.getdata()
-#         header = file.getheader()
+    # with CsvReader('good.csv') as file:
+    #     for i in range(len(file.getdata())):
+    #         print(file.getdata()[i], end = "\n")
 
     # with CsvReader('bad.csv') as file:
     #     if file == None:
     #         print("File is corrupted")
+    #     for i in range(len(file.getdata())):
+    #         print(file.getdata()[i], end = "\n")
