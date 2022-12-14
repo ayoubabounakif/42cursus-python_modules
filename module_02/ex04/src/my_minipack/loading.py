2b87ab566def858c36b5b31c65bde03e90f2ba61
ABOUNAKIF Ayoub <aabounakif@leyton.com>
2022-12-14 17:04:57 +0100

    [42-AI][M_02] Implement my_minipack package structure

---
+++ module_02/ex04/src/my_minipack/loading.py
@@ -0,0 +1,32 @@
+import time
+
+SCALE = 3333
+WIDTH = 60
+START = time.time()
+
+def ft_progress(listy):
+    for i in listy:
+        yield i
+
+def progress_bar(elem, ret):
+    left = WIDTH * ret // (SCALE * 2)
+    right = WIDTH - left
+    tags = "=" * left 
+    spaces = " " * right
+    estimated_time = (time.time() - START) * (SCALE) / (elem + 1)
+    percents = f"{ret / (SCALE / 100 * 2):.0f}%"
+    print("\rETA: {:.2f}s ".format(estimated_time), "[", percents, "][", tags, spaces, "] ", ret // 2,'/', SCALE, " | ", "elapsed time {:.2f}".format(time.time() - START), sep="", end="", flush=True)
+
+# def main():
+#     listy = range(SCALE)
+#     ret = 0
+#     for elem in ft_progress(listy):
+#         ret += (elem + 3) % 5
+#         progress_bar(elem, ret)
+#         time.sleep(0.005)
+#     print()
+#     print(ret)
+
+# if __name__ == '__main__':
+#     main()
+
