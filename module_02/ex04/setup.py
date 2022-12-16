b2bed316b7397928f7e7c6718668602aa026099e
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2022-12-16 14:23:33 +0100

    [42AI][M_02] Update README.md and Improve on configuration files

--- module_02/ex04/setup.py
+++ module_02/ex04/setup.py
@@ -1,15 +1,28 @@
 import setuptools as st
 
 st.setup(
-  name='my-minipack',
+  name='my_minipack',
   version='1.0.0',
   description='How to create a package in python.',
-  author='Ayoub ABOUNAKIF',
-)
-
-st.find_packages(
-  where='src',
-  include=['my_minipack'],
-  exclude=['tests'],
+  author='aabounak',
+  author_email='aabounak@student.1337.ma',
+  license='GPLv3',
+  packages=st.find_packages(
+    where='src',
+    include=['my_minipack'],
+    exclude=['tests'],
+  ),
+  python_requires='>=3.7',
+  classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Students",
+    "Topic :: Education",
+    "Topic :: HowTo",
+    "Topic :: Package",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only"
+  ],
 )
 
