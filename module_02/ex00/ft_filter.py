6c18bee0b6be74dcc84a4db9a0dc935726c0a356
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2022-12-17 14:53:12 +0100

    [42AI][M_02] Add space on docstring

--- module_02/ex00/ft_filter.py
+++ module_02/ex00/ft_filter.py
@@ -1,5 +1,5 @@
 def ft_filter(function_to_apply, iterable):
-    """Filter the result of function apply to all elements of the iterable.
+    """ Filter the result of function apply to all elements of the iterable.
         @function_to_apply: a function taking an iterable.
         @iterable: an iterable object (list, tuple, iterator).
         @return An iterable. None if the iterable can not be used by the function.
--- module_02/ex00/ft_reduce.py
+++ module_02/ex00/ft_reduce.py
@@ -1,5 +1,5 @@
 def ft_reduce(function_to_apply, iterable):
-    """Apply function of two arguments cumulatively.
+    """ Apply function of two arguments cumulatively.
         @function_to_apply: a function taking an iterable.
         @iterable: an iterable object (list, tuple, iterator).
         @return A value, of same type of elements in the iterable parameter.
