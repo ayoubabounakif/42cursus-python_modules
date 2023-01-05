9e464c601cf14c215015aa3121f8ee2cf022e391
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2023-01-05 19:48:52 +0100

    [42AI][M_03] Clean code using abstractions

--- module_03/ex04/Kmeans.py
+++ module_03/ex04/Kmeans.py
@@ -10,6 +10,48 @@ class KmeansClustering:
         self.max_iter = max_iter # number of max iterations to update the centroids
         self.centroids = [] # values of the centroids
 
+    def __calculate_L2_distance(self, matrix, centroids):
+        distances = []
+        for centroid in centroids:
+            distance = []
+            for data_point in matrix:
+                distance.append(np.sqrt(np.sum((data_point - centroid) ** 2)))
+            distances.append(distance)
+        distances = np.array(distances)
+        return distances
+
+    def __update_centroids(self, matrix, clusters):
+        for i in range(self.ncentroid):
+            self.centroids[i] = np.mean(matrix[clusters == i], axis=0)
+
+    def __assign_clusters(self, matrix):
+        distances = self.__calculate_L2_distance(matrix, self.centroids)
+        clusters = np.argmin(distances, axis=0)
+        return clusters
+
+    def __generate_random_centroids(self, matrix):
+        return matrix[np.random.randint(0, matrix.shape[0], self.ncentroid)]
+
+    def __plot(self, X, y):
+        if X.shape[1] == 2:
+            plt.scatter(X[:, 0], X[:, 1], c=y)
+            plt.scatter(self.centroids[:, 0], self.centroids[:, 1], c='red')
+        elif X.shape[1] == 3:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            for label, name in enumerate(REGISTERED_AREAS):
+                ax.text3D(X[y == label, 0].mean(),
+                        X[y == label, 1].mean() + 1.5,
+                        X[y == label, 2].mean(), name,
+                        horizontalalignment='center',
+                        bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
+            ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y)
+            ax.scatter(self.centroids[:, 0], self.centroids[:, 1], self.centroids[:, 2], c='red')
+        plt.show()
+
+
+
+
     def fit(self, X):
         """
         Run the K-means clustering algorithm.
@@ -27,22 +69,10 @@ class KmeansClustering:
         -------
         This function should not raise any Exception.
         """
-        
-        # Randomly initialize the centroids (0, X.shape[0], size=self.ncentroid)
-        self.centroids = X[np.random.randint(0, X.shape[0], self.ncentroid)]
+        self.centroids = self.__generate_random_centroids(X)
         for _ in range(self.max_iter):
-            # Assign each data point to the closest centroid
-            distances = []
-            for centroid in self.centroids:
-                distance = []
-                for data_point in X:
-                    distance.append(np.sqrt(np.sum((data_point - centroid) ** 2)))
-                distances.append(distance)
-            distances = np.array(distances)
-            self.clusters = np.argmin(distances, axis=0)
-            # Update the centroids to be the mean of the points assigned to them.
-            for i in range(self.ncentroid):
-                self.centroids[i] = np.mean(X[self.clusters == i], axis=0)
+            clusters = self.__assign_clusters(X)
+            self.__update_centroids(X, clusters)
         
     def predict(self, X):
         """
@@ -60,40 +90,20 @@ class KmeansClustering:
         -------
         This function should not raise any Exception.
         """
-        dist = np.array([np.linalg.norm(X - self.centroids[i], axis=1) for i in range(self.ncentroid)])
-        clusters = np.argmin(dist, axis=0)
-        return clusters
+        return self.__assign_clusters(X)
 
     def plot(self, X, y):
-        if X.shape[1] == 2:
-            plt.scatter(X[:, 0], X[:, 1], c=y)
-            plt.scatter(self.centroids[:, 0], self.centroids[:, 1], c='red')
-        elif X.shape[1] == 3:
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
-            for label, name in enumerate(REGISTERED_AREAS):
-                ax.text3D(X[y == label, 0].mean(),
-                        X[y == label, 1].mean() + 1.5,
-                        X[y == label, 2].mean(), name,
-                        horizontalalignment='center',
-                        bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
-            ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y)
-            ax.scatter(self.centroids[:, 0], self.centroids[:, 1], self.centroids[:, 2], c='red')
-        plt.show()
+        if X.shape[1] == 2 or X.shape[1] == 3:
+            self.__plot(X, y)
+        else:
+            print("Cannot plot data with more than 3 dimensions")
 
 def main():
-    """
-    Centroids should be randomly initialized
-
-    For a fix value of 'ncentroid', centroids coordinates slightly differ when running the algorithm multiple times.
-    
-    Results are coherent and consistent trough multiple program execution. Most of the time, centroids converged to similar coordinates and number people associated to each centroids varies little.
-    """
-    # kmeans = KmeansClustering()
-    # matrix = np.random.rand(100, 2)
-    # kmeans.fit(matrix)
-    # vector = kmeans.predict(matrix)
-    # kmeans.plot(matrix, vector)
+    kmeans = KmeansClustering()
+    matrix = np.random.rand(100, 2)
+    kmeans.fit(matrix)
+    vector = kmeans.predict(matrix)
+    kmeans.plot(matrix, vector)
 
     matrix = np.loadtxt(open(sys.argv[1].split('=')[1], 'rb'), delimiter=',', skiprows=1, usecols=range(1, 4))
     kmeans = KmeansClustering(ncentroid=int(sys.argv[2].split('=')[1]), max_iter=int(sys.argv[3].split('=')[1]))
