52f8d8ddc0acb0f9e6db02db6a70fe3053e14a37
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2023-01-05 17:14:54 +0100

    [42AI][M_03] Implement a KMeans Algorithm

--- module_03/ex04/Kmeans.py
+++ module_03/ex04/Kmeans.py
@@ -1,14 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import sys
 
 class KmeansClustering:
-    def __init__(self, max_iter=50, ncentroid=5):
+    def __init__(self, max_iter=100, ncentroid=5):
         self.ncentroid = ncentroid # number of centroids
         self.max_iter = max_iter # number of max iterations to update the centroids
         self.centroids = [] # values of the centroids
-
-        self.clusters = [] # cluster of each data point
 
     def fit(self, X):
         """
@@ -30,14 +28,16 @@ class KmeansClustering:
         # Randomly initialize the centroids (0, X.shape[0], size=self.ncentroid)
         self.centroids = X[np.random.randint(0, X.shape[0], self.ncentroid)]
         for _ in range(self.max_iter):
-            # Calculate the distance between each data point in 'X' and each centroid
-            # Distance is calculated using the Euclidean distance between two points
-            # -> The centroid that is closest to the data point is the cluster that the data point belongs to
-            distances = np.array([np.linalg.norm(X - c, axis=1) for c in self.centroids])
             # Assign each data point to the closest centroid
+            distances = []
+            for centroid in self.centroids:
+                distance = []
+                for data_point in X:
+                    distance.append(np.sqrt(np.sum((data_point - centroid) ** 2)))
+                distances.append(distance)
+            distances = np.array(distances)
             self.clusters = np.argmin(distances, axis=0)
-            print(self.clusters)
-            # Update the centroids
+            # Update the centroids to be the mean of the points assigned to them.
             for i in range(self.ncentroid):
                 self.centroids[i] = np.mean(X[self.clusters == i], axis=0)
         
@@ -57,21 +57,30 @@ class KmeansClustering:
         -------
         This function should not raise any Exception.
         """
-        # f
-        # dist = np.array([np.linalg.norm(X - self.centroids[i], axis=1) for i in range(self.ncentroid)])
-        # clusters = np.argmin(dist, axis=0)
-        # return clusters
-        return self.clusters
+        dist = np.array([np.linalg.norm(X - self.centroids[i], axis=1) for i in range(self.ncentroid)])
+        clusters = np.argmin(dist, axis=0)
+        return clusters
 
     def plot(self, X, y):
-        plt.scatter(X[:, 0], X[:, 1], c=y)
-        plt.scatter(self.centroids[:, 0], self.centroids[:, 1], c='red')
+        if X.shape[1] == 2:
+            plt.scatter(X[:, 0], X[:, 1], c=y)
+            plt.scatter(self.centroids[:, 0], self.centroids[:, 1], c='red')
+        elif X.shape[1] == 3:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y)
+            ax.scatter(self.centroids[:, 0], self.centroids[:, 1], self.centroids[:, 2], c='red')
         plt.show()
 
 if __name__ == "__main__":
-    kmeans = KmeansClustering()
+    # kmeans = KmeansClustering()
+    # matrix = np.random.rand(100, 2)
+    # kmeans.fit(matrix)
+    # vector = kmeans.predict(matrix)
+    # kmeans.plot(matrix, vector)
 
-    matrix = np.random.rand(100, 2)
+    matrix = np.loadtxt(open(sys.argv[1].split('=')[1], 'rb'), delimiter=',', skiprows=1, usecols=range(1, 4))
+    kmeans = KmeansClustering(ncentroid=int(sys.argv[2].split('=')[1]), max_iter=int(sys.argv[3].split('=')[1]))
     kmeans.fit(matrix)
     vector = kmeans.predict(matrix)
     kmeans.plot(matrix, vector)
