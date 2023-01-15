07b99b23b79e4008a1401c79b385dd956eb3a55b
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2023-01-15 18:32:04 +0100

    [42AI][M_04] Setup MyPlotLib class

---
+++ module_04/ex06/MyPlotLib.py
@@ -0,0 +1,24 @@
+import matplotlib as plt
+
+class MyPlotLib:
+
+    def __init__(self):
+        pass
+
+    def histogram(self, data, features):
+        pass
+
+    def density(self, data, features):
+        pass
+
+    def pair_plot(self, data, features):
+        pass
+
+    def box_plot(self, data, features):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
