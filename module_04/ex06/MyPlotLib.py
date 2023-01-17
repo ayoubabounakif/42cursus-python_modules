1ac516a9f4e06ca70eced856e3575ac8d7480bf1
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2023-01-17 11:56:04 +0100

    [42AI][M_04] Understand matplot

--- module_04/ex06/MyPlotLib.py
+++ module_04/ex06/MyPlotLib.py
@@ -1,4 +1,5 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy as np
 
 class MyPlotLib:
 
@@ -18,6 +19,22 @@ class MyPlotLib:
         pass
 
 def main():
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    # Create some data
+    x = np.linspace(0, 4*np.pi, 200)
+    y1 = np.sin(x)
+    y2 = 1.5*np.sin(x)
+    y3 = 2*np.sin(x)
+
+    ax.plot(x, y1, label='A = 1')
+    ax.plot(x, y2, label='A = 1.5')
+    ax.plot(x, y3, label='A = 2')
+
+    ax.legend()
+
+    plt.show()
     pass
 
 if __name__ == "__main__":
